d81d5ede5071200adfcfd783a5af6dd893aac016
Andrew Maksimchuk <amaksimcthuk@gmail.com>
2025-05-25 16:35:08 +0300

    change structure of properties.ts and index.ts

--- gencss.ts
+++ gencss.ts
@@ -1,10 +1,10 @@
-import { selector, margin, atrule } from ".";
+import { selector, property, atrule } from ".";
 
 console.log(
   selector(
     ".user",
-    margin("1px", "1px", "1px", "1px"),
-    margin("1px", "1px", "1px", "1px")
+    property.margin("1px", "1px", "1px", "1px"),
+    property.margin("1px", "1px", "1px", "1px")
   ),
   selector("#form"),
 
@@ -12,5 +12,7 @@ console.log(
   atrule.namespace("http://", "about"),
 
   selector(".card:hover", ""),
-  selector(".card:active", "dfsf:")
+  selector(".card:active", "dfsf:"),
+
+  selector("button.logout", "dsfdf:sdfsdf")
 );
--- properties.ts
+++ properties.ts
@@ -1,5 +1,64 @@
 import * as cssValues from "./values";
 
+interface Specification {
+  Name: string;
+  Value: string | number;
+  Initial: string;
+  "Applies to": string;
+  Inherited: string;
+  Percentages: string;
+  "Computed value": string;
+  "Canonical order": string;
+  "Animation type": string;
+}
+
+function builder<T extends Specification>(
+  name: T["Name"],
+  valueCounter: 1
+): (value: T["Value"]) => string;
+function builder<T extends Specification>(
+  name: T["Name"],
+  valueCounter: 2
+): (value: T["Value"], value2: T["Value"]) => string;
+function builder<T extends Specification>(
+  name: T["Name"],
+  valueCounter: 3
+): (value: T["Value"], value2: T["Value"], value3: T["Value"]) => string;
+function builder<T extends Specification>(
+  name: T["Name"],
+  valueCounter: 4
+): (
+  value: T["Value"],
+  value2: T["Value"],
+  value3: T["Value"],
+  value4: T["Value"]
+) => string;
+
+function builder<T extends Specification>(
+  name: T["Name"],
+  valueCounter: number
+) {
+  if (1 === valueCounter) return (value: T["Value"]) => `${name}: ${value};`;
+
+  if (2 === valueCounter)
+    return (value: T["Value"], value2: T["Value"]) =>
+      `${name}: ${value} ${value2};`;
+
+  if (3 === valueCounter)
+    return (value: T["Value"], value2: T["Value"], value3: T["Value"]) =>
+      `${name}: ${value} ${value2} ${value3};`;
+
+  if (4 === valueCounter)
+    return (
+      value: T["Value"],
+      value2: T["Value"],
+      value3: T["Value"],
+      value4: T["Value"]
+    ) => `${name}: ${value} ${value2} ${value3} ${value4};`;
+
+  throw new Error("Too many arguments");
+}
+
 type displayOutside = "block" | "inline" | "run-in";
 type displayInside = "flow" | "flow-root" | "table" | "flex" | "grid" | "ruby";
 type displayType = displayOutside | displayInside;
@@ -372,7 +431,11 @@ export interface anchorScope {
   "Animation type": "discrete";
 }
 
-type aspectRationValue = "auto" | cssValues.ratio | `auto ${cssValues.ratio}` | `${cssValues.ratio} auto`;
+type aspectRationValue =
+  | "auto"
+  | cssValues.ratio
+  | `auto ${cssValues.ratio}`
+  | `${cssValues.ratio} auto`;
 
 export interface aspectRatio {
   Name: "aspect-ratio";
@@ -387,11 +450,25 @@ export interface aspectRatio {
 }
 
 //[[ left-side | far-left | left | center-left | center | center-right | right | far-right | right-side ] || behind ]
-type azimuthValue = "left-side" | "far-left" | "left" | "center-left" | "center" | "center-right" | "right" | "far-right" | "right-side"
+type azimuthValue =
+  | "left-side"
+  | "far-left"
+  | "left"
+  | "center-left"
+  | "center"
+  | "center-right"
+  | "right"
+  | "far-right"
+  | "right-side";
 
 export interface azimuth {
   Name: "azimuth";
-  Value: cssValues.angle | azimuthValue | "leftwards" | "rightwards" | "inherit";
+  Value:
+    | cssValues.angle
+    | azimuthValue
+    | "leftwards"
+    | "rightwards"
+    | "inherit";
   Initial: "center";
   "Applies to": "all elements";
   Inherited: "yes";
@@ -401,3 +478,52 @@ export interface azimuth {
   "Animation type": "";
 }
 
+export const display = builder<display>("display", 1);
+export const boxSizing = builder<boxSizing>("box-sizing", 1);
+export const margin = builder<margin>("margin", 4);
+export const marginTop = builder<marginTop>("margin-top", 1);
+export const marginRight = builder<marginRight>("margin-right", 1);
+export const marginBottom = builder<marginBottom>("margin-bottom", 1);
+export const marginLeft = builder<marginLeft>("margin-left", 1);
+export const marginBlock = builder<marginBlock>("margin-block", 2);
+export const marginInline = builder<marginInline>("margin-inline", 2);
+export const marginBlockStart = builder<marginBlockStart>(
+  "margin-block-start",
+  1
+);
+export const marginBlockEnd = builder<marginBlockEnd>("margin-block-end", 1);
+export const marginInlineStart = builder<marginInlineStart>(
+  "margin-inline-start",
+  1
+);
+export const marginInlineEnd = builder<marginInlineEnd>("margin-inline-end", 1);
+export const marginBreak = builder<marginBreak>("margin-break", 1);
+export const marginTrim = builder<marginTrim>("margin-trim", 1);
+export const accentColor = builder<accentColor>("accent-color", 1);
+export const appearance = builder<appearence>("appearance", 1);
+
+/** The all property is a shorthand that resets all CSS properties except direction and unicode-bidi. It only accepts the CSS-wide keywords. It does not reset custom properties */
+export const all = builder<all>("all", 1);
+
+/** The align-content property aligns a flex container’s lines within the flex container when there is extra space in the cross-axis, similar to how justify-content aligns individual items within the main-axis. Note, this property has no effect on a single-line flex container. */
+export const alignContent = builder<alignContent>("align-content", 1);
+
+export const alignItems = builder<alignItems>("align-items", 1);
+export const alignSelf = builder<alignSelf>("align-self", 1);
+
+/** This property specifies the box’s alignment baseline: the baseline used to align the box prior to applying its post-alignment shift (if applicable). */
+export const alignmentBaseline = builder<alignmentBaseline>(
+  "alignment-baseline",
+  1
+);
+
+/** The anchor-name property declares that an element is an anchor element, whose principal box is an anchor box, and gives it a list of anchor names to be targeted by. */
+export const anchorName = builder<anchorName>("anchor-name", 1);
+
+/** This property scopes the specified anchor names, and lookups for these anchor names, to this element’s subtree. */
+export const anchorScope = builder<anchorScope>("anchor-scope", 1);
+
+/** This property sets a preferred aspect ratio for the box, which will be used in the calculation of auto sizes and some other layout functions. */
+export const aspectRatio = builder<aspectRatio>("aspect-ratio", 1);
+
+export const azimuth = builder<azimuth>("azimuth", 1);
